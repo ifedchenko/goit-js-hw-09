25c92dea2139a99b6edb32753eeb5ffb72da29c1
Yriy Fedchenko <y.fedchenko@gmail.com>
2023-05-12 21:02:32 +0300

    finish task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,43 @@
+import Notiflix from 'notiflix';
+
+const form = document.querySelector('.form');
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const delay = parseInt(event.target.elements.delay.value);
+  const step = parseInt(event.target.elements.step.value);
+  const amount = parseInt(event.target.elements.amount.value);
+
+  let currentDelay = delay;
+  for (let i = 1; i <= amount; i++) {
+    createPromise(i, currentDelay).then(
+      ({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+        // console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      },
+      ({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+        // console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      }
+    );
+    currentDelay += step;
+  }
+});

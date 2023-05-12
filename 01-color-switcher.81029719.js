dc567f02c65f1ebe73713a4e5b9a7958007c6dfb
ifedchenko <99099982+ifedchenko@users.noreply.github.com>
2023-05-12 12:49:08 +0000

    Deploying to gh-pages from @ ifedchenko/goit-js-hw-09@0da74fd3d7ce2660b0008859e6f327a99644ee4b 🚀

--- 01-color-switcher.81029719.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");let r;t.addEventListener("click",(function(){const d=document.querySelector("body");r=setInterval((()=>{d.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3),t.setAttribute("disabled",!0),e.removeAttribute("disabled")})),e.setAttribute("disabled",!0),e.addEventListener("click",(function(){clearInterval(r),t.removeAttribute("disabled"),e.setAttribute("disabled",!0)}));
-//# sourceMappingURL=01-color-switcher.81029719.js.map
